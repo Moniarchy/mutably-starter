90b92ac82727380a87f93f0530baeda9979f76d7
Monica Williams <monicaestellaw@gmail.com>
2017-12-19 12:53:36 -0800

    Updates to GET fetch
    The fetch now only runs when the get pokemon button is clicked. Yay!

--- public/script.js
+++ public/script.js
@@ -2,6 +2,11 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+  //selects the buttons to toggle the fetch get and post requests, respectively
+  const getAllPokemonButton = document.querySelectorAll('.get-pokemon-button')[0]
+  const addAPokemonButton = document.querySelectorAll('.add-pokemon-button')[0]
+
+  //This gets used inside the fetch below that makes a get request
   const showPokemon = function(data) {
     const displaySection = document.querySelectorAll('.list')[0]
 
@@ -19,6 +24,7 @@ $(document).ready(function(){
     })
   }
 
+  //options for the fetch to take
   const initObject = {
     method: 'GET'
     // headers:
@@ -28,14 +34,30 @@ $(document).ready(function(){
     // referrer:
   }
 
-  fetch("https://mutably.herokuapp.com/pokemon", initObject)
-  .then(function(data) {
-    return data.json()
+  //this button triggers a fetch that makes a post request
+  addAPokemonButton.addEventListener('click', function() {
+    //hide the list of pokemon
+    //show the form
+    console.log('bye')
   })
-  .then(function(pokemonObjects) {
-    showPokemon(pokemonObjects)
-  })
-  .catch(function(error) {
-    console.log(error)
+
+  //this button triggers a fetch that makes a get request
+  getAllPokemonButton.addEventListener('click', function() {
+    //hide the eventual form
+    fetch("https://mutably.herokuapp.com/pokemon", initObject)
+    // .then(function(data) {
+    //   if (!data.ok) {
+    //     throw data.statusText
+    //   }
+    // })
+    .then(function(data) {
+      return data.json()
+    })
+    .then(function(pokemonObjects) {
+      showPokemon(pokemonObjects)
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
   })
 });
